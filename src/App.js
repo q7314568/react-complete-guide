5e2734a382ff1e67390aec146f1a501c95c4062d
q7314568 <q7314568@gmail.com>
2021-03-24 02:52:50 +0800

    兩種呼叫帶參數function的方式

--- src/App.js
+++ src/App.js
@@ -1,51 +1,49 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-const app = (props) => {
-  const [personState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: "Peter", age: 35 },
       { name: "Bob", age: 23 },
     ],
-  });
-
-  const switchPersons = () => {
-    setPersonsState({ persons: personState.persons.reverse() });
   };
 
-  return (
-    <div className="App">
-      <h1>Hi,I'am a React app</h1>
-      <button onClick={switchPersons}>switch person</button>
-      <Person
-        name={personState.persons[0].name}
-        age={personState.persons[0].age}
-      >
-        My hobbies is VideoGame{" "}
-      </Person>
-      <Person
-        name={personState.persons[1].name}
-        age={personState.persons[1].age}
-      />
-    </div>
-  );
-  // return React.createElement(
-  //   "div",
-  //   { className: "app" },
-  //   React.createElement("h1", null, "Hi,I'am a React app")
-  // );
-};
+  switchPersons = (newName) => {
+    // this.setState({ persons: this.state.persons.reverse() });
+    this.setState({
+      persons: [
+        { name: "Peter", age: 35 },
+        { name: newName, age: 23 }
+      ],
+    });
+  };
 
-export default app;
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi,I'am a React app</h1>
+        <button onClick={()=>this.switchPersons('Tyler')}>switch person</button>
+        <Person
+          click={this.switchPersons.bind(this,'Tyler!!')}
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
+          My hobbies is VideoGame
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+      </div>
+    );
+    // return React.createElement(
+    //   "div",
+    //   { className: "app" },
+    //   React.createElement("h1", null, "Hi,I'am a React app")
+    // );
+  }
+}
 
-// state={
-//   persons:[
-//     {name:"Peter",age:35},
-//     {name:"Bob",age:23}
-//   ]
-// }
-
-// switchPersons=()=>{
-//   this.setState({persons:this.state.persons.reverse()});
-// }
+export default App;
