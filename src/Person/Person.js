6c76b1d10d58e330e6c4b73d29b7e831485a370c
q7314568 <q7314568@gmail.com>
2021-03-24 15:31:22 +0800

    為Person加入css

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from 'react';
- 
+import './Person.css'
 
 const person =(props)=>{
     // console.log(props);
     return (
-    <div>
+    <div className="Person">
         <p onClick={props.click}>{props.children}</p>
         <p>My name is {props.name} and I am {props.age} years old!</p>
         <input type="text" onChange={props.changed} value={props.name}></input>
